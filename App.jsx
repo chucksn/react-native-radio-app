da5b26ebc8d1a18d6787c082fd781f081ebf0526
Chucks Nwosu <chucksn611@gmail.com>
2023-03-09 00:31:33 -0600

    more codes

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text_1}>World Radio</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text_1: {
+    color: "white",
+    fontSize: 32,
+  },
+});
